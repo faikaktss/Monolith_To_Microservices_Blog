0eb076376193202f72c33ac4d206fab5a98d6da7
𝐅𝐚𝐢𝐤 𝐀𝐤𝐭𝐚𝐬 <148656104+faikaktss@users.noreply.github.com>
2025-11-18 16:46:55 +0300

    CommentController Tamamlandı

--- src/main/java/com/faik/demo2/dto/CommentDto.java
+++ src/main/java/com/faik/demo2/dto/CommentDto.java
@@ -1,6 +1,5 @@
 package com.faik.demo2.dto;
 
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
---
+++ src/main/java/com/faik/demo2/dto/TagDto.java
@@ -0,0 +1,15 @@
+package com.faik.demo2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagDto {
+    
+    private Long id;
+    private String name;
+    private String slug;
+}
